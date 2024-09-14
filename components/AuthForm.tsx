3415319a283b8407b38ff8d230fe0e901221d170
u-nikk <abhinav.te21@bmsce.ac.in>
2024-09-14 17:01:08 +0530

    Completed Auth part

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,6 +22,9 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react'
+import SignUp from '@/app/(auth)/sign-up/page'
+import { useRouter } from 'next/navigation'
+
 
 
 const formSchema = z.object({
@@ -31,25 +34,47 @@ const formSchema = z.object({
 
 
 const AuthForm = ({ type }: { type: string }) => {
+    const router = useRouter();
     const [user, setuser] = useState(null);
     const [isLoading, setisLoading] = useState(false);
 
+    const formSchema = authFormSchema(type);
+
     // 1. Define your form.
-    const form = useForm<z.infer<typeof authFormSchema>>({
-        resolver: zodResolver(authFormSchema),
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
         defaultValues: {
             email: "",
-            password:""
+            password: ""
         },
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof authFormSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
         setisLoading(true)
-        console.log(values)
-        setisLoading(false)
+
+        // try {
+        //     //Sign up with Appwrite & plain link token
+
+        //     if(type === 'sign-up'){
+        //         // const newUser = await signUp(data);
+
+        //         // setuser(newUser);
+        //     }
+
+        //     if(type==='sign-in'){
+        //         const response = await SignIn({
+        //             email:data.email,
+        //             password:data.password,
+        //         })
+        //         if(response) router.push('/')
+        //     }
+        // } catch (error) {
+        //     console.log(error);
+        // } finally{
+        //     setisLoading(false);
+        // }
+        
     }
 
     return (
@@ -94,7 +119,7 @@ const AuthForm = ({ type }: { type: string }) => {
 
             </header>
 
-            
+
             {user ? (
                 <div className='flex flex-col gap-4'>
                     {/*PlaidLink*/}
@@ -104,9 +129,29 @@ const AuthForm = ({ type }: { type: string }) => {
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
 
-                        <CustomInput control={form.control} name='email'  label = 'Email' placeholder = 'Enter your email'/>
-                        <CustomInput control={form.control} name='password'  label = 'Password' placeholder = 'Enter your password'/>
-                        
+                            {type === 'sign-up' && (
+                                <>
+                                    <div className='flex gap-4'>
+                                        <CustomInput control={form.control} name='firstName' label='First Name' placeholder='Enter your first name' />
+                                        <CustomInput control={form.control} name='lastName' label='Last Name' placeholder='Enter your last name' />
+                                    </div>
+                                    <CustomInput control={form.control} name='address1' label='Address' placeholder='Enter your specific address' />
+                                    <CustomInput control={form.control} name='city' label='City' placeholder='Enter your City' />
+                                    <div className='flex gap-4'>
+                                        <CustomInput control={form.control} name='state' label='State' placeholder='Enter your state' />
+                                        <CustomInput control={form.control} name='pinCode' label='PIN Code' placeholder='Example 111111' />
+                                    </div>
+                                    <div className='flex gap-4'>
+                                        <CustomInput control={form.control} name='dateOfBirth' label='Date of Birth' placeholder='YYYY-MM-DD' />
+                                        <CustomInput control={form.control} name='ssn' label='SSN' placeholder='Example: 1234' />
+                                    </div>
+
+                                </>
+                            )}
+
+                            <CustomInput control={form.control} name='email' label='Email' placeholder='Enter your email' />
+                            <CustomInput control={form.control} name='password' label='Password' placeholder='Enter your password' />
+
                             {/* <FormField 
                                 control={form.control}
                                 name="email"
@@ -132,8 +177,8 @@ const AuthForm = ({ type }: { type: string }) => {
                                 )}
                             /> */}
 
-            
-                                
+
+
                             {/* <FormField
                                 control={form.control}
                                 name="password"
@@ -159,15 +204,37 @@ const AuthForm = ({ type }: { type: string }) => {
                                     </div>
                                 )}
                             /> */}
-                            <Button className='form-btn' type="submit" disabled={isLoading}>{isLoading ? (
-                                <>
-                                    <Loader2 size={20} className='animate-spin'/> &nbsp;
-                                    Loading...
-                                </>    
-                        ) : type === 'sign-in' ? 'Sign in' : 'Sign up'
-                    }</Button>
+
+                            <div className='flex flex-col gap-4'>
+                                <Button className='form-btn' type="submit" disabled={isLoading}>{isLoading ? (
+                                    <>
+                                        <Loader2 size={20} className='animate-spin' /> &nbsp;
+                                        Loading...
+                                    </>
+                                ) : type === 'sign-in' ? 'Sign in' : 'Sign up'
+                                }
+                                </Button>
+                            </div>
                         </form>
                     </Form>
+
+                    <footer className='flex justify-center gap-1'>
+
+                        <p className='text-14 font-normal text-grey-600'>
+                            {type === 'sign-in'
+                                ? "Don't have an account?"
+                                : "Already have an account?"
+                            }
+                        </p>
+
+                        <Link href=
+                            {type === 'sign-in' ? '/sign-up' :
+                                "/sign-in"} className='form-link'>
+                            {type === 'sign-in' ? "Sign Up" :
+                                "Sign In"}
+                        </Link>
+
+                    </footer>
 
                 </>
             )
--- lib/utils.ts
+++ lib/utils.ts
@@ -195,8 +195,18 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = z.object({
+export const authFormSchema = (type: string) => z.object({
+  // sign up
+  firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(30),
+  pinCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  // both
   email: z.string().email(),
-  password:z.string().min(8)
+  password: z.string().min(8),
 })
  
